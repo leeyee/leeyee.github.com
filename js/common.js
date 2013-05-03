280517f72fd3676669de1df701f800ac522186f1
leeyee <seadlead@gmail.com>
2013-05-04 00:03:56 +0800

    modify

--- js/common.js
+++ js/common.js
@@ -1,6 +1,16 @@
 $(function() {
+	// 代码高亮
 	$("pre").addClass("prettyprint  linenums");
 	$.getScript('/js/prettify/prettify.js', function() {
 		prettyPrint();
+	});
+
+	// 文章的链接做弹出式访问
+	$.each($("#j_article a"), function(idx, ele) {
+		var href = $(ele).attr("href");
+		if (href && href.indexOf("#") != 0) {
+			$(ele).attr("target", "_blank");
+			$(ele).append("<i class='icon-share'>");
+		}
 	});
 });
