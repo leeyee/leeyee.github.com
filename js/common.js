61e79cf1172b29fba31df78384e92036a40cc21b
leeyee <seadlead@gmail.com>
2015-09-07 19:09:21 +0800

    prettify style

--- js/common.js
+++ js/common.js
@@ -1,13 +1,11 @@
 $(function () {
     // 代码高亮
-    $("pre").addClass("prettyprint  linenums");
-    $.getScript('/js/prettify/prettify.js', function () {
+    $("pre").addClass("prettyprint linenums");
+    $.getScript('http://apps.bdimg.com/libs/prettify/r298/prettify.min.js', function () {
         prettyPrint();
     });
-
     // 表格
     $("table").addClass("table table-bordered");
-
     // 文章的链接做弹出式访问
     $.each($("#j_article a"), function (idx, ele) {
         var href = $(ele).attr("href");
