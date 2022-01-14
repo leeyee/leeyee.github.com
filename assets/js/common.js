c25d3db5168bb6013c9fa081c8a0cc960d60f5de
leeyee <seadlead@sina.com>
2022-01-14 14:24:37 +0800

    use bulma style instead of spectre style

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,78 +1,83 @@
+const SHOW_ANCHOR_OFFSET = 40;
+
 $(function () {
 
-    MathJax.Extension.MathZoom = true;
-    MathJax.Extension.MathMenu = true;
+  //MathJax.Extension.MathZoom = true;
+  //MathJax.Extension.MathMenu = true;
 
-    $("pre").addClass('code');
+  // 目录
+  const $markdownToc = $('#markdown-toc');
+  const $rightToc = $('#article_toc');
 
-    $('#markdown-toc').addClass('nav');
-    $('#markdown-toc ul').addClass('nav');
-    $("#markdown-toc li").addClass('nav-item text-ellipsis');
-    $("#markdown-toc").wrap('<div id="default-toc" class="toast"></div>');
-    $('#default-toc').prepend(' <label>目录</label><button id="close-toc" class="btn btn-clear float-right"></button></div>');
-    $("#tableOfContent-btn").click(function () {
-        $("#default-toc").toggle('slow');
+  let isHideRightToc = () => {
+    return $rightToc.css('display') === 'none';
+  }
+
+  if ($markdownToc.length) {
+    $markdownToc.addClass("menu-list is-size-7");
+    $markdownToc.find('ul').addClass('menu-list');
+    $rightToc.find("p:first").after($markdownToc);
+  } else {
+    $rightToc.find("p:first").remove();
+  }
+
+  // 表格
+  $("article.content table").addClass("table is-bordered is-narrow");
+
+  // 文章的链接做弹出式访问
+  $.each($("article.content a"), function (idx, ele) {
+    const href = $(ele).attr("href");
+    const _target = $(ele).attr("target");
+    if (href && href.indexOf("#") != 0 && !_target) {
+      $(ele).attr("target", "_blank");
+    }
+  });
+
+  // 收集锚点位置信息。只收集一次，不考虑页面变化导致的锚点位置变化的问题。
+  const anchorMap = new Map();
+  $.each($markdownToc.find('a'), function (idx, ele) {
+    const anchorName = decodeURIComponent($(ele).attr("href"));
+    const anchorOffset = $(anchorName).offset().top;
+    anchorMap.set(anchorOffset, anchorName.substr(1));
+  });
+
+  // 添加滚动事件。到达锚点附近时点亮右边对应的目录标题
+  if (anchorMap.size) {
+    // 缓存当前已经点亮的标题
+    let currentAnchorOffset = 0;
+    const anchors = [...anchorMap.keys()];
+    $(document).bind('scroll', function () {
+      const scrollTop = $(this).scrollTop();
+      if (isHideRightToc() || Math.abs(currentAnchorOffset - scrollTop) < SHOW_ANCHOR_OFFSET) {
+        return;
+      }
+      const targetAnchors = anchors.filter(anchorOffset => Math.abs(anchorOffset - scrollTop) < SHOW_ANCHOR_OFFSET);
+      if (targetAnchors.length) {
+        console.info("Reach Anchor", targetAnchors);
+        currentAnchorOffset = targetAnchors[0];
+        const topAnchorName = anchorMap.get(targetAnchors[0]);
+        $rightToc.find('a').removeClass("is-active");
+        $(`#article_toc #markdown-toc-${topAnchorName}`).addClass("is-active");
+      }
     });
-    $('#default-toc #close-toc').click(function () {
-        $("#default-toc").hide('slow');
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
     });
-
-    // 表格
-    $("table").addClass("table table-hover table-scroll");
-
-    // 文章的链接做弹出式访问
-    $.each($("#article_content a"), function (idx, ele) {
-        const href = $(ele).attr("href");
-        const _target = $(ele).attr("target");
-        if (href && href.indexOf("#") != 0 && !_target) {
-            $(ele).attr("target", "_blank");
-        }
-    });
-
-    // When to show the scroll link
-    // higher number = scroll link appears further down the page
-    const upperLimit = 200;
-
-    // Our scroll link element
-    const scrollElem = $('#toTop');
-
-    // Scroll to top speed
-    const scrollSpeed = 500;
-
-    // Show and hide the scroll to top link based on scroll position
-    scrollElem.hide();
-
-    $(window).scroll(function () {
-        const scrollTop = $(document).scrollTop();
-        if (scrollTop > upperLimit) {
-            $(scrollElem).stop().fadeTo(300, 1); // fade back in
-        } else {
-            $(scrollElem).stop().fadeTo(300, 0); // fade out
-        }
-    });
-
-    // Scroll to top animation on click
-    $(scrollElem).click(function () {
-        $('html, body').animate({scrollTop: 0}, scrollSpeed);
-        return false;
-    });
-
-    $('#copyrightModal_btn').click(function () {
-        $('#copyrightModal').addClass("active");
-    });
-
-    $("#copyrightModal .btn-link").click(function () {
-        $('#copyrightModal').removeClass("active");
-    });
-
-    $("#profile_tabs .panel-nav li").click(function(){
-
-        $('#profile_tabs .panel-nav li').removeClass('active');
-        $('#profile_tabs .panel-body').addClass('d-hide');
-
-        const tab_id = $(this).data('tab-id');
-        $(this).addClass('active');
-        $("#"+tab_id).removeClass('d-hide');
-    })
-
+  }
 });
