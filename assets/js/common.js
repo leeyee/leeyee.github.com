5a4255bfbff1d648da58feae7822d0ad01fec278
leeyee <seadlead@sina.com>
2022-01-20 12:41:33 +0800

    Skin by bulma (#10)
    use new skin bulma

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,78 +1,126 @@
+'use strict';
+
+const SHOW_ANCHOR_OFFSET = 40;
+const LIGHT_CSS_CLASSES="has-text-primary-dark has-text-weight-bold";
+
+const toggleGoTop = (scrollTop) => {
+  if (scrollTop > 50) {
+    $(".go-top").removeClass('is-hidden');
+  } else {
+    $(".go-top").addClass("is-hidden");
+  }
+};
+
+const addScrollEventListener = (func) => {
+  $(document).bind('scroll', func);
+};
+
+const removeScrollEventListener = () => {
+  $(document).unbind('scroll');
+}
+
 $(function () {
 
-    MathJax.Extension.MathZoom = true;
-    MathJax.Extension.MathMenu = true;
+  const $article = $("article.content");
 
-    $("pre").addClass('code');
+  // 表格
+  $article.find("table").addClass("table is-bordered is-narrow");
 
-    $('#markdown-toc').addClass('nav');
-    $('#markdown-toc ul').addClass('nav');
-    $("#markdown-toc li").addClass('nav-item text-ellipsis');
-    $("#markdown-toc").wrap('<div id="default-toc" class="toast"></div>');
-    $('#default-toc').prepend(' <label>目录</label><button id="close-toc" class="btn btn-clear float-right"></button></div>');
-    $("#tableOfContent-btn").click(function () {
-        $("#default-toc").toggle('slow');
+  // 文章的链接做弹出式访问
+  $.each($article.find("a"), function (idx, ele) {
+    const href = $(ele).attr("href");
+    const _target = $(ele).attr("target");
+    if (href && href.indexOf("#") != 0 && !_target) {
+      $(ele).attr("target", "_blank");
+    }
+  });
+
+  // 目录
+  const $markdownToc = $('#markdown-toc');
+  const $rightToc = $('#article_toc');
+
+  let isHideRightToc = () => {
+    return $rightToc.css('display') === 'none';
+  }
+
+  if ($markdownToc.length) {
+    $markdownToc.addClass("menu-list is-size-7");
+    $markdownToc.find('ul').addClass('menu-list');
+    $rightToc.find("p:first").after($markdownToc);
+  } else {
+    $rightToc.find("p:first").remove();
+  }
+
+  // 收集锚点位置信息。只收集一次，不考虑页面变化导致的锚点位置变化的问题。
+  const anchorMap = new Map();
+  $.each($markdownToc.find('a'), function (idx, ele) {
+    const anchorName = decodeURIComponent($(ele).attr("href"));
+    const anchorOffset = $(anchorName).offset().top;
+    anchorMap.set(anchorOffset, anchorName.substr(1));
+  });
+
+  // 缓存当前已经点亮的标题
+  let currentAnchorOffset = 0;
+  const anchors = [...anchorMap.keys()];
+
+  const lightAnchor = (scrollTop) => {
+    if (isHideRightToc() || Math.abs(currentAnchorOffset - scrollTop) < SHOW_ANCHOR_OFFSET) {
+      return;
+    }
+    const targetAnchors = anchors.filter(anchorOffset => Math.abs(anchorOffset - scrollTop) < SHOW_ANCHOR_OFFSET);
+    if (targetAnchors.length) {
+      console.debug("Reach Anchor", targetAnchors);
+      currentAnchorOffset = targetAnchors[0];
+      const topAnchorName = anchorMap.get(targetAnchors[0]);
+      $rightToc.find('a').removeClass(LIGHT_CSS_CLASSES);
+      $(`#article_toc #markdown-toc-${topAnchorName}`).addClass(LIGHT_CSS_CLASSES);
+    }
+  };
+
+  const scrollHandler = () => {
+    const scrollTop = $(this).scrollTop();
+    toggleGoTop(scrollTop);
+    if (anchorMap.size) {
+      lightAnchor(scrollTop);
+    }
+  }
+
+  // 添加滚动事件。到达锚点附近时点亮右边对应的目录标题
+  addScrollEventListener(scrollHandler);
+
+  // 重置右侧导航为初始状态
+  const resetRightToc = () => {
+    $rightToc.find('a').removeClass(LIGHT_CSS_CLASSES);
+    removeScrollEventListener();
+    currentAnchorOffset = 0;
+  };
+
+  // 回到顶部
+  $('.go-top').bind('click', function (e) {
+    e.preventDefault();
+    resetRightToc();
+    $('html').animate({scrollTop: 0}, 'slow', () => {
+      addScrollEventListener(scrollHandler);
     });
-    $('#default-toc #close-toc').click(function () {
-        $("#default-toc").hide('slow');
+  });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
     });
-
-    // 表格
-    $("table").addClass("table table-hover table-scroll");
-
-    // 文章的链接做弹出式访问
-    $.each($("#article_content a"), function (idx, ele) {
-        const href = $(ele).attr("href");
-        const _target = $(ele).attr("target");
-        if (href && href.indexOf("#") != 0 && !_target) {
-            $(ele).attr("target", "_blank");
-        }
-    });
-
-    // When to show the scroll link
-    // higher number = scroll link appears further down the page
-    const upperLimit = 200;
-
-    // Our scroll link element
-    const scrollElem = $('#toTop');
-
-    // Scroll to top speed
-    const scrollSpeed = 500;
-
-    // Show and hide the scroll to top link based on scroll position
-    scrollElem.hide();
-
-    $(window).scroll(function () {
-        const scrollTop = $(document).scrollTop();
-        if (scrollTop > upperLimit) {
-            $(scrollElem).stop().fadeTo(300, 1); // fade back in
-        } else {
-            $(scrollElem).stop().fadeTo(300, 0); // fade out
-        }
-    });
-
-    // Scroll to top animation on click
-    $(scrollElem).click(function () {
-        $('html, body').animate({scrollTop: 0}, scrollSpeed);
-        return false;
-    });
-
-    $('#copyrightModal_btn').click(function () {
-        $('#copyrightModal').addClass("active");
-    });
-
-    $("#copyrightModal .btn-link").click(function () {
-        $('#copyrightModal').removeClass("active");
-    });
-
-    $("#profile_tabs .panel-nav li").click(function(){
-
-        $('#profile_tabs .panel-nav li').removeClass('active');
-        $('#profile_tabs .panel-body').addClass('d-hide');
-
-        const tab_id = $(this).data('tab-id');
-        $(this).addClass('active');
-        $("#"+tab_id).removeClass('d-hide');
-    })
-
+  }
 });
--- assets/js/puzzle.js
+++ assets/js/puzzle.js
@@ -1,265 +1,267 @@
+'use strict';
+
 class Utils {
 
-    /**
-     * 获取指定范围内的随机数
-     * @param min
-     * @param max
-     * @returns {number} r -> [min, max)
-     */
-    static getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min;
-    }
+  /**
+   * 获取指定范围内的随机数
+   * @param min
+   * @param max
+   * @returns {number} r -> [min, max)
+   */
+  static getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
 
-    /**
-     * 获取数组的逆序数
-     * @param arr
-     * @returns {number}
-     */
-    static getNumberInversions(arr) {
-        let t = 0;
-        for (let i = 1; i < arr.length; i++) {
-            for (let j = 0; j < i; j++) {
-                if (arr[j] > arr[i]) {
-                    t++;
-                }
-            }
+  /**
+   * 获取数组的逆序数
+   * @param arr
+   * @returns {number}
+   */
+  static getNumberInversions(arr) {
+    let t = 0;
+    for (let i = 1; i < arr.length; i++) {
+      for (let j = 0; j < i; j++) {
+        if (arr[j] > arr[i]) {
+          t++;
         }
-        return t;
+      }
     }
+    return t;
+  }
 
 
-    /**
-     * 使用 Knuth Durstenfeld 洗牌算法打乱数组
-     * @param arr
-     */
-    static knuthDurstenfeldShuffle(arr) {
-        let len = arr.length;
-        while (len > 1) {
-            const randomIndex = Utils.getRandomInt(0, len - 1);
-            console.log(len, "exchange: ", randomIndex, len - 1);
-            [arr[len - 1], arr[randomIndex]] = [arr[randomIndex], arr[len - 1]];
-            console.log("[0,", len, "] = ", randomIndex, "-->", arr, "逆序数:", Utils.getNumberInversions(arr));
-            --len;
-        }
+  /**
+   * 使用 Knuth Durstenfeld 洗牌算法打乱数组
+   * @param arr
+   */
+  static knuthDurstenfeldShuffle(arr) {
+    let len = arr.length;
+    while (len > 1) {
+      const randomIndex = Utils.getRandomInt(0, len - 1);
+      console.log(len, "exchange: ", randomIndex, len - 1);
+      [arr[len - 1], arr[randomIndex]] = [arr[randomIndex], arr[len - 1]];
+      console.log("[0,", len, "] = ", randomIndex, "-->", arr, "逆序数:", Utils.getNumberInversions(arr));
+      --len;
     }
+  }
 
-    /**
-     * 是否是奇数
-     * @param number
-     * @returns {boolean}
-     */
-    static isOdd(number) {
-        return number % 2 !== 0;
-    }
+  /**
+   * 是否是奇数
+   * @param number
+   * @returns {boolean}
+   */
+  static isOdd(number) {
+    return number % 2 !== 0;
+  }
 }
 
 class Point {
-    constructor(x, y) {
-        this.X = x;
-        this.Y = y;
-    }
+  constructor(x, y) {
+    this.X = x;
+    this.Y = y;
+  }
 }
 
 class Puzzle {
 
-    constructor(customProps) {
+  constructor(customProps) {
 
-        const props = {
-            W: 140,
-            MATRIX_N: 3,
-        };
+    const props = {
+      W: 140,
+      MATRIX_N: 3,
+    };
 
-        this.props = {...props, ...customProps};
+    this.props = {...props, ...customProps};
 
-        this.cPoint = new Point(this.props.W / 2, this.props.W / 2);
+    this.cPoint = new Point(this.props.W / 2, this.props.W / 2);
 
-        this.numbers = this.getSolvablePuzzleNumbers();
+    this.numbers = this.getSolvablePuzzleNumbers();
+  }
+
+  /**
+   * 获取有解数列
+   * @returns {*[]}
+   */
+  getSolvablePuzzleNumbers() {
+    let arr = Array.from(new Array(Math.pow(this.props.MATRIX_N, 2) - 1), (val, index) => index + 1);
+
+    Utils.knuthDurstenfeldShuffle(arr);
+
+    if (Utils.isOdd(this.props.MATRIX_N)) {
+      [arr[3], arr[1]] = [arr[1], arr[3]];
     }
 
-    /**
-     * 获取有解数列
-     * @returns {*[]}
-     */
-    getSolvablePuzzleNumbers() {
-        let arr = Array.from(new Array(Math.pow(this.props.MATRIX_N, 2) - 1), (val, index) => index + 1);
+    return [...arr, 'N'];
+  }
 
-        Utils.knuthDurstenfeldShuffle(arr);
+  getIndex(number) {
+    return this.numbers.findIndex(v => {
+      return v === number;
+    });
+  }
 
-        if (Utils.isOdd(this.props.MATRIX_N)) {
-            [arr[3], arr[1]] = [arr[1], arr[3]];
-        }
+  getOffset(number) {
+    const index = this.getIndex(number);
+    const row = Math.floor(index / this.props.MATRIX_N);
+    const col = index % this.props.MATRIX_N;
+    return {R: row, C: col};
+  }
 
-        return [...arr, 'N'];
-    }
-
-    getIndex(number) {
-        return this.numbers.findIndex(v => {
-            return v === number;
-        });
-    }
-
-    getOffset(number) {
-        const index = this.getIndex(number);
-        const row = Math.floor(index / this.props.MATRIX_N);
-        const col = index % this.props.MATRIX_N;
-        return {R: row, C: col};
-    }
-
-    getPoint(number) {
-        const offset = this.getOffset(number);
-        const x = this.cPoint.X + offset.C * this.props.W;
-        const y = this.cPoint.Y + offset.R * this.props.W;
-        return new Point(x, y);
-    }
+  getPoint(number) {
+    const offset = this.getOffset(number);
+    const x = this.cPoint.X + offset.C * this.props.W;
+    const y = this.cPoint.Y + offset.R * this.props.W;
+    return new Point(x, y);
+  }
 }
 
 class PuzzleGame {
 
-    constructor(canvas, puzzle, customProps) {
-        const props = {};
+  constructor(canvas, puzzle, customProps) {
+    const props = {};
 
-        this.canvas = canvas;
-        this.puzzle = puzzle;
-        this.props = {...props, customProps};
+    this.canvas = canvas;
+    this.puzzle = puzzle;
+    this.props = {...props, customProps};
+  }
+
+  _canExchange(number) {
+    const nullOffset = this.puzzle.getOffset('N');
+
+    const numberOffset = this.puzzle.getOffset(number);
+
+    return nullOffset.R === numberOffset.R && Math.abs(nullOffset.C - numberOffset.C) === 1 ||
+      nullOffset.C === numberOffset.C && Math.abs(nullOffset.R - numberOffset.R) === 1;
+
+  }
+
+  doExchange(number) {
+    if (!this._canExchange(number)) {
+      return;
     }
 
-    _canExchange(number) {
-        const nullOffset = this.puzzle.getOffset('N');
+    const nullIndex = this.puzzle.getIndex('N');
+    const numberIndex = this.puzzle.getIndex(number);
 
-        const numberOffset = this.puzzle.getOffset(number);
+    this.puzzle.numbers[nullIndex] = number;
+    this.puzzle.numbers[numberIndex] = 'N';
 
-        return nullOffset.R === numberOffset.R && Math.abs(nullOffset.C - numberOffset.C) === 1 ||
-            nullOffset.C === numberOffset.C && Math.abs(nullOffset.R - numberOffset.R) === 1;
+    const nullPoint = this.puzzle.getPoint('N');
+    const numberPoint = this.puzzle.getPoint(number);
 
-    }
+    this.canvas.setLayerGroup(`group#${number}`, {
+      x: numberPoint.X,
+      y: numberPoint.Y,
+    });
+    this.canvas.setLayerGroup(`group#N`, {
+      x: nullPoint.X,
+      y: nullPoint.Y,
+    });
 
-    doExchange(number) {
-        if (!this._canExchange(number)) {
-            return;
+    this.canvas.drawLayers({
+      complete: () => {
+        this.doJudgment();
+      }
+    });
+  }
+
+  doJudgment() {
+    const len = Math.pow(this.puzzle.props.MATRIX_N, 2);
+    if (this.puzzle.numbers.includes('N', -1)) {
+      let isCompleted = true;
+      let baseNumber = this.puzzle.numbers[0];
+      for (let i = 1; i < len - 1; i++) {
+        if (this.puzzle.numbers[i] - baseNumber !== 1) {
+          isCompleted = false;
+          break;
+        } else {
+          baseNumber = this.puzzle.numbers[i];
         }
+      }
+      if (isCompleted) {
+        this.renderCompleted('Congratulations!');
+      }
+    }
+  }
 
-        const nullIndex = this.puzzle.getIndex('N');
-        const numberIndex = this.puzzle.getIndex(number);
+  render() {
+    this.puzzle.numbers.map(number => {
 
-        this.puzzle.numbers[nullIndex] = number;
-        this.puzzle.numbers[numberIndex] = 'N';
+      const nPoint = this.puzzle.getPoint(number);
 
-        const nullPoint = this.puzzle.getPoint('N');
-        const numberPoint = this.puzzle.getPoint(number);
+      this.canvas.drawRect({
+        data: {number: number},
+        layer: true,
+        name: `name#${number}`,
+        groups: [`group#${number}`],
+        x: nPoint.X,
+        y: nPoint.Y,
+        strokeStyle: '#00d1b2',
+        strokeWidth: 1,
+        fillStyle: () => {
+          return number === 'N' ? '#FFF' : '#FFF';
+        },
+        width: this.puzzle.props.W,
+        height: this.puzzle.props.W,
+        click: (layer) => {
+          console.log('click layer: ', layer);
+          this.doExchange(layer.data.number);
+        },
+        touchend: (layer) => {
+          this.doExchange(layer.data.number);
+        },
+      });
 
-        this.canvas.setLayerGroup(`group#${number}`, {
-            x: numberPoint.X,
-            y: numberPoint.Y,
+      if (number !== 'N') {
+        this.canvas.drawText({
+          layer: true,
+          name: `label#${number}`,
+          fillStyle: '#363636',
+          x: nPoint.X,
+          y: nPoint.Y,
+          fontSize: 48,
+          fontFamily: 'Verdana, sans-serif',
+          text: number,
+          groups: [`group#${number}`],
         });
-        this.canvas.setLayerGroup(`group#N`, {
-            x: nullPoint.X,
-            y: nullPoint.Y,
-        });
+      }
 
-        this.canvas.drawLayers({
-            complete: () => {
-                this.doJudgment();
-            }
-        });
-    }
+    });
+  }
 
-    doJudgment() {
-        const len = Math.pow(this.puzzle.props.MATRIX_N, 2);
-        if (this.puzzle.numbers.includes('N', -1)) {
-            let isCompleted = true;
-            let baseNumber = this.puzzle.numbers[0];
-            for (let i = 1; i < len - 1; i++) {
-                if (this.puzzle.numbers[i] - baseNumber !== 1) {
-                    isCompleted = false;
-                    break;
-                } else {
-                    baseNumber = this.puzzle.numbers[i];
-                }
-            }
-            if (isCompleted) {
-                this.renderCompleted('Congratulations!');
-            }
-        }
-    }
+  renderCompleted(text) {
+    const x = this.puzzle.props.W * this.puzzle.props.MATRIX_N / 2;
+    const cPoint = new Point(x, x);
+    this.canvas.drawRect({
+      layer: true,
+      fillStyle: 'gray',
+      x: 0,
+      y: 0,
+      width: this.puzzle.props.W * this.puzzle.props.MATRIX_N,
+      height: this.puzzle.props.W * this.puzzle.props.MATRIX_N,
+      fromCenter: false
+    })
+      .drawText({
+        layer: true,
+        text: text,
+        fontSize: 30,
+        fontFamily: 'Verdana, sans-serif',
+        fillStyle: '#36c',
+        x: cPoint.X,
+        y: cPoint.Y,
+      });
+  }
 
-    render() {
-        this.puzzle.numbers.map(number => {
-
-            const nPoint = this.puzzle.getPoint(number);
-
-            this.canvas.drawRect({
-                data: {number: number},
-                layer: true,
-                name: `name#${number}`,
-                groups: [`group#${number}`],
-                x: nPoint.X,
-                y: nPoint.Y,
-                strokeStyle: 'red',
-                strokeWidth: 1,
-                fillStyle: () => {
-                    return number === 'N' ? '#FFF' : '#FFF';
-                },
-                width: this.puzzle.props.W,
-                height: this.puzzle.props.W,
-                click: (layer) => {
-                    console.log('click layer: ', layer);
-                    this.doExchange(layer.data.number);
-                },
-                touchend: (layer) => {
-                    this.doExchange(layer.data.number);
-                },
-            });
-
-            if (number !== 'N') {
-                this.canvas.drawText({
-                    layer: true,
-                    name: `label#${number}`,
-                    fillStyle: '#36c',
-                    x: nPoint.X,
-                    y: nPoint.Y,
-                    fontSize: 48,
-                    fontFamily: 'Verdana, sans-serif',
-                    text: number,
-                    groups: [`group#${number}`],
-                });
-            }
-
-        });
-    }
-
-    renderCompleted(text) {
-        const x = this.puzzle.props.W * this.puzzle.props.MATRIX_N / 2;
-        const cPoint = new Point(x, x);
-        this.canvas.drawRect({
-            layer: true,
-            fillStyle: 'gray',
-            x: 0,
-            y: 0,
-            width: this.puzzle.props.W * this.puzzle.props.MATRIX_N,
-            height: this.puzzle.props.W * this.puzzle.props.MATRIX_N,
-            fromCenter: false
-        })
-            .drawText({
-                layer: true,
-                text: text,
-                fontSize: 48,
-                fontFamily: 'Verdana, sans-serif',
-                fillStyle: '#36c',
-                x: cPoint.X,
-                y: cPoint.Y,
-            });
-    }
-
-    clearCanvas() {
-        let layerNames = [];
-        this.canvas.getLayers(layer => {
-            layerNames.push(layer.name);
-        })
-        layerNames.map(name => {
-            this.canvas.removeLayer(name);
-        })
-        this.canvas.drawLayers();
-    }
+  clearCanvas() {
+    let layerNames = [];
+    this.canvas.getLayers(layer => {
+      layerNames.push(layer.name);
+    })
+    layerNames.map(name => {
+      this.canvas.removeLayer(name);
+    })
+    this.canvas.drawLayers();
+  }
 }
